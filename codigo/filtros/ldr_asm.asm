fa4dd0d826e8808a3c89a8637ac8b655de5def63
Franco Cesar Schiavinato <fschiavinato@ws4.labo5>
2016-04-21 21:50:40 -0300

    Casi nada ecambios, ciclo del ldr en asm, continuar

--- codigo/filtros/ldr_asm.asm
+++ codigo/filtros/ldr_asm.asm
@@ -1,7 +1,7 @@
 %define pXi 4
-%define 
+%define max 4876875
 global ldr_asm
-
+%define bs_word 16
 
 
 section .text
@@ -26,79 +26,53 @@ ldr_asm:
 	mov rbp, rsp
 	mov r10, [rbp+16]
 	push r12
+	push r13
 	xor r12, r12
 	mov r12d, ecx
-	sub r12, 4
-
 	xor rax, rax;fila
 	xor r11, r11;columna
-	.loopnotocararab:
-		movdqu xmm0, [rdi+r11*16]
-		movdqu [rsi+r11*16], xmm0
-		add r11, pXi
-		cmp r11, edx
-		jl .loopnotocararab
-		xor r11,r11
-		add edi, r8d
-		add esi, r9d
-		inc rax
-		cmp rax, 2
-		jl .loopnotocararab
-		je .loopcambios
-		cmp eax, ecx
-		jl .loopnotocararab
-		je .fin
-	.loopcambios:
-		cmp r11, 0
-		je .cambioizq
-		cmp r11d, r12d
-		je .cambioder
-		movdqu xmm0, [rdi+r11*16]; xmm0 = | a0 | r0 | g0 | b0 | a1 | r1 | g1 | b1 | a2 | r2 | g2 | b2 | a3 | r3 | g3 | b3 |
-		movdqu xmm1, [rdi+r11*16-8]
-		movdqu xmm2, [rdi+r11*16-4]
-		movdqu xmm3, [rdi+r11*16+4]
-		movdqu xmm4, [rdi+r11*16+8]
-		paddb xmm0, xmm1
-		paddb xmm0, xmm2
-		paddb xmm0, xmm3
-		paddb xmm0, xmm4
-		movdqu xmm1, [rdi+r11*16-8]
-		movdqu xmm2, [rdi+r11*16-4]
-		movdqu xmm3, [rdi+r11*16+4]
-		movdqu xmm4, [rdi+r11*16+8]
-		paddb xmm0, xmm1
-		paddb xmm0, xmm2
-		paddb xmm0, xmm3
-		paddb xmm0, xmm4
-		movdqu xmm1, [rdi+r11*16-8]
-		movdqu xmm2, [rdi+r11*16-4]
-		movdqu xmm3, [rdi+r11*16+4]
-		movdqu xmm4, [rdi+r11*16+8]
-		paddb xmm0, xmm1
-		paddb xmm0, xmm2
-		paddb xmm0, xmm3
-		paddb xmm0, xmm4
-		movdqu xmm1, [rdi+r11*16-8]
-		movdqu xmm2, [rdi+r11*16-4]
-		movdqu xmm3, [rdi+r11*16+4]
-		movdqu xmm4, [rdi+r11*16+8]
-		paddb xmm0, xmm1
-		paddb xmm0, xmm2
-		paddb xmm0, xmm3
-		paddb xmm0, xmm4
-		movdqu xmm1, [rdi+r11*16-8]
-		movdqu xmm2, [rdi+r11*16-4]
-		movdqu xmm3, [rdi+r11*16+4]
-		movdqu xmm4, [rdi+r11*16+8]
-		paddb xmm0, xmm1
-		paddb xmm0, xmm2
-		paddb xmm0, xmm3
-		paddb xmm0, xmm4
+	xor r13, r13 
+	mov r13d, edx
+	sub r13d, 4 ; le resto a r13d los 4 finales de cols a los que no accedo
+	sub r12d, 4; le resto a r12d los 4 finales de fils a los que no accedo
+	pxor xmm8, xmm8
+	; Avazamos el contador de filas, hasta tener  src_row_size - 4. Reiniciamos el contador
+	; xmm0 es el regisro donde se guardan los pixelesa procesar actualmente.	
+	.ciclodefilas:
+	cmp eax, r12d
+	JE .nuevafila:
+	movdqu xmm0, [rdi + r8d*2 + 16] ; me muevo a la posicion del medio de dos filas más abajo
+	movdqa xmm1, xmm0 ; xmm1 = xmm0
+	JMP .suma
+	inc eax
+	JMP .ciclodefilas 
+
+	.nuevafila
+	xor eax, eax ; vuelvo a la primera fila
+	cmp r11, r13d
+	JE .fin; 
 
 
+	.suma:	
+    ; movdqu xmm8, mascara, con mascara = aun no lo he hecho. mascara = | a0 | 0 | 0 | 0 | a1 | 0 | 0 | 0 | a2 | 0 | 0 | 0 | a3 | 0 | 0 | 0 |
+	psrld xmm0, bs_byte ; xmm0 = | 0 | r0 | g0 | b0 | 0 | r1 | g1 | b1 | 0 | r2 | g2 | b2 | 0 | r3 | g3 | b3 | 
+    
+
+
+
+
+
+
+	
 
 	.fin:
+
+	 	pop r13
 		pop r12
 		pop rbp
 		ret
  
+
+
+
+; el resultado de suma rgb,  tiene que estar en 32 bits
